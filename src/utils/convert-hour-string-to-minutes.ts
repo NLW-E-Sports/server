5842714b26b077c978a21a478b8daf8a91410a7c
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-18 08:15:10 -0300

    feat :sparkles: created routes for games and ads

---
+++ src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,9 @@
+const convertHourStringToMinutes = (hourString: string) => {
+  const [hours, minutes] = hourString.split(":").map(Number);
+
+  const minutesAmount = hours * 60 + minutes;
+
+  return minutesAmount;
+};
+
+export default convertHourStringToMinutes;
